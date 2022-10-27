9eeba61e1e5f5efa4077b3bfdcd3b5d833b6e863
pablo.cristini <pablitoocristini@gmail.com>
2022-10-27 19:42:54 -0300

    ultimo commit

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 class Usuario {
     'nombre' = ""
     'apellido' = ""
-    'mascotas' = []
     'libros' = []
-    constructor(nombre, apellido, mascotas){
+    'mascotas' = []
+    constructor(nombre, apellido, mascotas, Libro){
         this.nombre = nombre;
         this.apellido = apellido;
-        this.mascotas = mascotas;
+        this.mascotas = mascotas; 
+        this.libros = Libro; 
     }
     getFullName = () => {
         console.log(`El usuario es: ${this.nombre} ${this.apellido}`);
     }
     addMascota = () => {
-        let nuevaMascota = Usuario.mascotas.push();
+        Usuario.mascotas.push();
+        console.log("mascotas agregada")
     }
     countMascota = () => {
         let cantidadDeMascotas = Usuario.mascotas.lentgh()
         console.log(cantidadDeMascotas);
     }
     addBook = (nombre, autor) => {
-        let libro = new Libro (nombre, autor)
-        this.libros.add(Libro)
-        console.log(`el usuario le gusta ${libro.nombre} de ${libro.autor}`)
+        let libro = new Libro (nombre, autor)  
+        console.log(`Al usuario le gusta ${libro.nombre} de ${libro.autor}`)
     }
     getBookNames = (libros) => {
         let arrayLibros = array.from(Usuario.libros)
@@ -36,9 +37,11 @@ class Libro {
     }
 }
 
-let pablo = new Usuario("pablo", "cristini", [], []);
+let pablo = new Usuario("pablo", "cristini");
 pablo.getFullName()
 pablo.addBook("Harry Potter", "fedklen")
+pablo.addMascota("perro")
+
 
 
 
